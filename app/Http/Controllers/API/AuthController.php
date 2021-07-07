d1738442626a183155fb53ff0fbd8ef42554141d
elameer <a4hb.fci@gmail.com>
2021-07-07 14:35:39 +0200

    install laravel sanctum & authcontroller signin ,signup, signout & adminseeder

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,60 @@
+<?php
+   
+namespace App\Http\Controllers\API;
+   
+use Illuminate\Http\Request;
+use App\Http\Controllers\API\BaseController as BaseController;
+use Illuminate\Support\Facades\Auth;
+use Validator;
+use App\Models\User;
+   
+class AuthController extends BaseController
+{
+
+    public function signin(Request $request)
+    {
+        if(Auth::attempt(['email' => $request->email, 'password' => $request->password])){ 
+            $authUser = Auth::user(); 
+            $success['token'] =  $authUser->createToken('MyAuthApp')->plainTextToken; 
+            $success['user'] =  $authUser;
+   
+            return $this->sendResponse($success, 'User signed in');
+        } 
+        else{ 
+            return $this->sendError('Unauthorised.', ['error'=>'Cradential error']);
+        } 
+    }
+
+    public function signup(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required',
+            'confirm_password' => 'required|same:password',
+        ]);
+   
+        if($validator->fails()){
+            return $this->sendError('Error validation', $validator->errors());       
+        }
+   
+        $input = $request->all();
+        $user = User::create($input);
+        $success['token'] =  $user->createToken('MyAuthApp')->plainTextToken;
+        $success['name'] =  $user->name;
+   
+        return $this->sendResponse($success, 'User created successfully.');
+    }
+
+    public function me(Request $request)
+    {
+        return $request->user();
+    }
+
+    public function logout(Request $request)
+    {
+        auth()->user()->tokens()->delete();
+        return $this->sendResponse($success, 'Sign Out.');
+    }
+   
+}
