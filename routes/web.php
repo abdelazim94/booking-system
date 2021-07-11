50707cde15a12665e82e0360edfecd565ef67b64
elameer <a4hb.fci@gmail.com>
2021-07-11 22:51:30 +0200

    add Grimzy\LaravelMysqlSpatial & place model

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Models\Place;
 use Illuminate\Support\Facades\Route;
 
+use Grimzy\LaravelMysqlSpatial\Types\Point;
+use Grimzy\LaravelMysqlSpatial\Types\Polygon;
+use Grimzy\LaravelMysqlSpatial\Types\LineString;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,7 +18,20 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    $place1 = new Place();
+    $place1->name = 'Empire State Building';
 
-Route::view('forgot_password', 'auth.reset_password')->name('password.reset');
+    // saving a point
+    $place1->location = new Point(40.7484404, -73.9878441);	// (lat, lng)
+    $place1->save();
+
+    // saving a polygon
+    $place1->area = new Polygon([new LineString([
+        new Point(40.74894149554006, -73.98615270853043),
+        new Point(40.74848633046773, -73.98648262023926),
+        new Point(40.747925497790725, -73.9851602911949),
+        new Point(40.74837050671544, -73.98482501506805),
+        new Point(40.74894149554006, -73.98615270853043)
+    ])]);
+    $place1->save();
+});
