de2c85b347aef10048e93c33c3ef93c7aedf4dfe
elameer <a4hb.fci@gmail.com>
2021-07-11 02:59:36 +0200

    add baseRepository for global transactions

---
+++ app/Repository/Eloquent/BaseRepository.php
@@ -0,0 +1,45 @@
+<?php
+namespace App\Repository\Eloquent;
+
+use EloquentRepositoryInterface;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Model;
+
+class BaseRepository implements EloquentRepositoryInterface
+{
+    protected  $model;
+
+    public function __construct(Model $model)
+    {
+        $this->model = $model;
+    }
+
+    public function all(array $columns = ['*'], array $relations = []): Collection
+    {
+        return $this->model->with($relations)->get($columns);
+    }
+
+    public function findById(int $modelId, array $columns = ["*"], array $relations = [], $appends=[]): ?Model
+    {
+        return $this->model->select($columns)->with($relations)->append($appends)->findorFail($modelId);
+    }
+
+    public function create(array $payload) : ?Model
+    {
+        $model = $this->model->create($payload);
+        return $model->fresh();
+    }
+
+    public function update(int $modelId, array $payload): bool
+    {
+        $model = $this->findById($modelId);
+        return $model->update($payload);
+    }
+
+    public function deleteById(int $modelId): bool
+    {
+        return $this->findById($modelId)->delete();
+    }
+
+
+}
---
+++ app/Repository/EloquentRepositoryInterface.php
@@ -0,0 +1,24 @@
+<?php
+namespace App\Repository;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Model;
+
+interface EloquentRepositoryInterface
+{
+    public function all(array $columns=['*'], array $relations=[]): Collection;
+
+    public function paginate($total=5);
+
+    public function findById(
+        int $modelId,
+        array $columns=["*"],
+        array $relations = [],
+        array $appends = []
+    ) : ?Model;
+
+    public function create(array $payload): ?Model;
+
+    public function update(int $modelId, array $payload):bool;
+
+    public function deleteById(int $modelId): bool;
+}
