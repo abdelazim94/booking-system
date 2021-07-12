f1cd23af01e37d73b83c6fedeb7c046b7e2cc03d
elameer <a4hb.fci@gmail.com>
2021-07-12 11:15:13 +0200

    create SlotController & SlotResource

---
+++ app/Http/Controllers/API/SlotController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Models\Doctor;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use App\Http\Controllers\API\BaseController as BaseController;
+use App\Http\Resources\SlotResource;
+
+class SlotController extends BaseController
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Doctor $doctor)
+    {
+        return $this->sendResponse($data= new SlotResource($doctor->slots()), __('lang.slots'), Response::HTTP_OK);
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
